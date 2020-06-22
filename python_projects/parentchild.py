695ab435dadec43d9c37ddaeefe54774553705a2
avass90 <60933123+avass90@users.noreply.github.com>
2020-06-22 07:44:14 -0600

    upload parent/child assignment
    step 174

---
+++ python_projects/parentchild.py
@@ -0,0 +1,18 @@
+class Deal:
+    deal_number: 4567
+    deal_startdate:1/1/2020
+    deal_enddate:6/30/2020
+    deal_value:250000
+
+class IO(Deal):
+    io_startdate:4/1/2020
+    io_enddate:4/30/2020
+    io_value:41666.67
+
+class Line(Deal):
+    line_startdate:4/1/2020
+    line_enddate:4/7/2020
+    line_value:9615.38
+
+
+    
